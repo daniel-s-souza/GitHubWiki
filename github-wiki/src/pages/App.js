47c9ca19bd36ca9373ee5063d59b1a7faa08419f
daniel-s-souza <danielsantsouza94@gmail.com>
2022-11-22 15:41:23 -0300

    feat: implementa inicio do input e sua estilização

--- github-wiki/src/pages/App.js
+++ github-wiki/src/pages/App.js
@@ -1,9 +1,11 @@
+import Input from '../components/input';
 import img from '../img/github.png';
 import {Container} from './styles'
 function App() {
   return (
     <Container>
-   <img src={img} alt="gitLogo" width={72} height={72}/>
+      <img src={img} alt="gitLogo" width={72} height={72}/>
+      <Input />
     </Container>
   );
 }
