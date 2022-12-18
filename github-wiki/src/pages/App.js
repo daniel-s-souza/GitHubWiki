43c4070dc16d6842e70a72759e0a3bf0fb10aea1
daniel-s-souza <danielsantsouza94@gmail.com>
2022-12-18 09:02:29 -0300

    feat: implementa pages

--- github-wiki/src/pages/App.js
+++ github-wiki/src/pages/App.js
@@ -1,11 +1,40 @@
-import Input from '../components/input';
-import img from '../img/github.png';
-import {Container} from './styles'
+import { useState } from "react";
+import Button from "../components/button/button";
+import Input from "../components/input/input";
+import Repositorios from "../components/repositorios/repositorios";
+import api from "../services/api";
+import Container from "./styles";
+
 function App() {
+
+  const [ currentRepo, setCurrentRepo ] = useState("")
+  const [ repos, setRepos ] = useState([]);
+
+  const handleSearchRepo = async () => {
+    const {data} = await api.get(`${currentRepo}`)
+
+    if (data.id) {
+      const repoExists = repos.find(repo => repo.id === data.id)
+
+      if (!repoExists) {
+        setRepos (prev => [...prev, data])
+        setCurrentRepo("")
+        return
+      } else {
+        alert("Repositório já adicionado.")
+      }
+    } else {
+      alert ("Este repositório não foi encontrado. Por favor, tente novamente.")
+    }
+  }
+
   return (
     <Container>
-      <img src={img} alt="gitLogo" width={72} height={72}/>
-      <Input />
+      <img src="https://www.nicepng.com/png/full/52-520535_free-files-github-github-icon-png-white.png" alt="Github Logo" />
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
+      <Button onClick={handleSearchRepo}/>
+      {repos.map((repo) => <Repositorios repo={repo}/>)}
+      
     </Container>
   );
 }
--- github-wiki/src/pages/styles.js
+++ github-wiki/src/pages/styles.js
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  margin: 20px 0;
-  width: 100%;
-  height: 90vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
+const Container = styled.section `
+    width: 100%;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 3.2rem 0;
+    img {
+        width: 85px;
+        height: 85px;
+        margin-bottom: 2.4rem;
+        transition: 0.5s ease-in-out;
+        &:hover {
+            scale: 1.1;
+        }
+    }
 `
+
+export default Container
